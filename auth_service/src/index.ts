189e3336f29b546f02a70a083baddb08f06382a2
angelmanuel1995 <angelmanuel.goez@hotmail.es>
2023-03-25 07:02:32 -0500

    All the CRUD operations defined #2

--- auth_service/src/index.ts
+++ auth_service/src/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello world!!!")
+import express, { Request, Response } from 'express'
+const server = express();
+
+const users: { [key: string  | number ]: any } = {}
+
+server.get('/api', (req: Request, res: Response) => {
+  res.send(users)
+})
+
+server.get('/api/:id', (req: Request, res: Response) => {
+  res.send(users[req.params.id])
+})
+
+server.post('/api', (req: Request, res: Response) => {
+  console.log(req.body, 'Este es el body que enviamos desde la petición')
+  const id = Math.ceil((Math.random() * 10000 ))
+  users[id] = {
+    id,
+    ...req.body
+  }
+  res.send(users[id])
+})
+
+server.patch('/api', (req: Request, res: Response) => {
+  res.send({message: '[PATCH] Hello world!!!'})
+})
+
+server.delete('/api/:id', (req: Request, res: Response) => {
+  const { id } = req.params
+  delete users[+id]
+})
+
+server.listen(80, () => {
+  console.log('App is up and running')
+})
