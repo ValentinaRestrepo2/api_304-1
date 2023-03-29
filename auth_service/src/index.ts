107e59bb80309cd346157a66e62619820742d1e0
angelmanuel1995 <angelmanuel.goez@hotmail.es>
2023-03-29 11:16:37 -0500

    Deleting no api get routes

--- auth_service/src/index.ts
+++ auth_service/src/index.ts
@@ -15,30 +15,6 @@ server.get('/api/:id', (req: Request, res: Response) => {
   res.send(users[req.params.id])
 })
 
-server.post('/api', (req: Request, res: Response) => {
-  const id = Math.ceil((Math.random() * 10000 ))
-  users[id] = {
-    id,
-    ...req.body
-  }
-  res.send(users[id])
-})
-
-server.patch('/api/:id', (req: Request, res: Response) => {
-  const { id } = req.params
-  users[id] = {
-    id,
-    ...req.body
-  }
-  res.send(users[id])
-})
-
-server.delete('/api/:id', (req: Request, res: Response) => {
-  const { id } = req.params
-  delete users[id]
-  res.send()
-})
-
 server.listen(80, () => {
   console.log('App is up and running')
 })
